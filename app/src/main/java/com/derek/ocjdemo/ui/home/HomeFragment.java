800cac4f7363421fe259fb6596086437ba4c3bc6
laihaihan <763433963@qq.com>
2016-10-17 11:27:20 +0800

    添加注释，修改了布局

--- app/src/main/java/com/derek/ocjdemo/ui/home/HomeFragment.java
+++ app/src/main/java/com/derek/ocjdemo/ui/home/HomeFragment.java
@@ -4,7 +4,7 @@ import com.derek.ocjdemo.R;
 import com.derek.ocjdemo.base.BaseFragment;
 
 /**
- *首页fragment主界面分工会常常名村民你觉得女尿检大军嘻嘻嘻
+ *首页fragment主界面
  */
 public class HomeFragment extends BaseFragment {
 
