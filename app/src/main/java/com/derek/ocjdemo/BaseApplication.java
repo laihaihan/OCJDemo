c650ec49e1bd9a3b39244391e60a6f57582a126e
laihaihan <763433963@qq.com>
2016-10-17 18:10:23 +0800

    添加注释，修改了主页面fragment的tablayout资源文件

---
+++ app/src/main/java/com/derek/ocjdemo/BaseApplication.java
@@ -0,0 +1,18 @@
+package com.derek.ocjdemo;
+
+import android.app.Application;
+
+
+
+/**
+ * Created by asus on 2016/10/17 15 37.
+ * 邮箱:763433963@qq.com
+ */
+
+public class BaseApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+}
