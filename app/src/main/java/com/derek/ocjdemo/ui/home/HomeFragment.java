80d7513172e4cf293d213399c4be353e29618cc8
duyb <1763233351@qq.com>
2016-10-17 11:04:31 +0800

    首页fragmrnt新增注释

--- app/src/main/java/com/derek/ocjdemo/ui/home/HomeFragment.java
+++ app/src/main/java/com/derek/ocjdemo/ui/home/HomeFragment.java
@@ -1,18 +1,10 @@
 package com.derek.ocjdemo.ui.home;
 
-import android.content.Context;
-import android.net.Uri;
-import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
 import com.derek.ocjdemo.R;
 import com.derek.ocjdemo.base.BaseFragment;
 
 /**
- *
+ *首页fragment
  */
 public class HomeFragment extends BaseFragment {
 
